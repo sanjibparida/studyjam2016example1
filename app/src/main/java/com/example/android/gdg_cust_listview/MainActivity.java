30753cf453b227b0d25c5d90dceab009e3c06873
Sanjib Parida <sanjib@Sanjibs-MacBook-Pro.local>
2016-02-08 14:23:12 +0530

    first commit

---
+++ app/src/main/java/com/example/android/gdg_cust_listview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.gdg_cust_listview;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ListActivity {
+
+    String[] presidents = {
+            "Dwight D. Eisenhower",
+            "John F. Kennedy",
+            "Lyndon B. Johnson",
+            "Richard Nixon",
+            "Gerald Ford",
+            "Jimmy Carter",
+            "Ronald Reagan",
+            "George H. W. Bush",
+            "Bill Clinton",
+            "George W. Bush",
+            "Barack Obama"
+    };
+
+    Integer[] imageIDs = {
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.pic7,
+            R.drawable.pic8,
+            R.drawable.pic9,
+            R.drawable.pic10,
+            R.drawable.pic11
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        AdvancedCustomArrayAdapter adapter =new AdvancedCustomArrayAdapter(this, presidents, imageIDs);
+        setListAdapter(adapter);
+
+    }
+
+
+}
